581f2172c562729a5a98a653b2abefbc6c44a6cf
GaelleBescond <118830153+julienB700@users.noreply.github.com>
2022-11-22 17:52:58 +0100

    Tribulles

---
+++ Tictactoe/Tictactoe.py
@@ -0,0 +1,21 @@
+#Exo1 Création des emplacements qui seront remplacé par les X et O
+
+#Ligne1
+c11= "_ "
+c12= "_ "
+c13= "_"
+
+#Ligne2
+c21= "_ "
+c22= "_ "
+c23= "_"
+#Ligne3
+c31= "_ "
+c32= "_ "
+c33= "_"
+
+print(c11 + c12 + c13)
+print(c21 + c22 + c23)
+print(c31 + c32 + c33)
+
+#Exo2 Fonction O X
--- Tribulles/Tribulles.py
+++ Tribulles/Tribulles.py
@@ -1,1 +1,37 @@
-myTable= [HHJH]
+#Importation du random nécessaire (pour être sur que la liste soit bien mélanger;) )
+from random import*
+
+Mouv= 0
+myTable= [3,45,18,12,30,29,23,20]
+
+
+
+for n in range (1:8):
+    print('*',end='')
+for i in range(1:8):
+    print('*')
+
+
+#List random Generator
+for i in range(0,len(myTable)):
+    myTable [i] = random.radint(0,100)
+print("List to sort")
+print (myTable)
+
+#Creation de la boucle pour changer les chiffres plusieurs fois jusqu'au resultat attendu
+for n in range(0,len(myTable)):
+    for i in range (i,len(myTable)-1):
+    if (myTable[i]>[i+1]):
+        Mouv= myTable [i]
+        myTable [i]= myTable [i+1]
+        myTable [i+1]= Mouv
+print("Sorted list")
+print(myTable)
+
+#Plus le programme avance plus il y a d'information à gérer
+#Les itérarions se déculplent aux fur et à mesures des passages dans la boucle.
+
+
+
+
+
